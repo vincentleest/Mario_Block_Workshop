da9173bbdebc71c35315e871efa0c760ecb23809
Vincent <vincent@test.(none)>
2013-03-23 20:42:28 -0400

    Vertical movement works! :D

--- MovementProgram/MarioBlockMovement/MarioBlockMovement.ino
+++ MovementProgram/MarioBlockMovement/MarioBlockMovement.ino
@@ -10,8 +10,8 @@ void handleShroomState();
 
 //OUTPUT PIN ASSIGNMENTS
 const int SHROOM_HOR_DIR_PIN = 0;
-const int COIN_MOTOR_POWER_PIN = 4;
-const int FLOWER_MOTOR_POWER_PIN = 1;
+const int COIN_MOTOR_POWER_PIN = 13;
+const int FLOWER_MOTOR_POWER_PIN = 11;
 const int SHROOM_HOR_POWER_PIN = 3;
 const int SHROOM_VER_POWER_PIN = 2;
 
@@ -22,8 +22,9 @@ const int COIN_PUSH_PIN = 12;//7;
 const int FLOWER_PUSH_PIN = 8;
 const int SHROOM_PUSH_PIN = 9;
 
-const unsigned long COIN_INTERVAL = 3000;
-const unsigned long FLOWER_INTERVAL = 3000;
+const unsigned long COIN_INTERVAL = 6000;
+const unsigned long FLOWER_INTERVAL = 6000;
+const unsigned long SHROOM_INTERVAL = 6000;
 
 const bool LEFT = HIGH;
 const bool RIGHT = LOW;
@@ -86,9 +87,10 @@ void setup() {
   pinMode(SHROOM_SENSOR_LEFT_PIN, INPUT_PULLUP);
   pinMode(SHROOM_SENSOR_RIGTH_PIN, INPUT_PULLUP);
 
-  digitalWrite(SHROOM_HOR_POWER_PIN, HIGH);
   machineCoinState == STATE_COIN_DOWN;
   digitalWrite(COIN_MOTOR_POWER_PIN, HIGH); //Stop coin motor.
+  digitalWrite(SHROOM_VER_POWER_PIN, HIGH); //Stop coin motor.
+  digitalWrite(SHROOM_HOR_POWER_PIN, HIGH); //Stop coin motor.
   digitalWrite(FLOWER_MOTOR_POWER_PIN, HIGH); //Stop coin motor.
 }
 
@@ -97,56 +99,91 @@ void loop() {
   unsigned long flowerCurrentMill = millis();
   unsigned long shroomCurrentMill = millis();
 
-  if ( digitalRead(COIN_PUSH_PIN)){
-    Serial.println("coin button pushed");
-  }else{
-    Serial.println("coin button not pushed");
-  }
+//  if ( digitalRead(COIN_PUSH_PIN)){
+//    Serial.println("coin button pushed");
+//  }else{
+//    Serial.println("coin button not pushed");
+//  }
 
   if(digitalRead(COIN_PUSH_PIN) && machineCoinState == STATE_COIN_DOWN){
     // Move the coin up only when the coin is down.
-    Serial.println("Coin button pushed move coin up");
+    //Serial.println("Coin button pushed move coin up");
     machineCoinState = STATE_COIN_UP;
     coinPreviousMillis = millis();
-    digitalWrite(SHROOM_VER_POWER_PIN, LOW);
+    digitalWrite(COIN_MOTOR_POWER_PIN, LOW);
   }
 
   if(machineCoinState == STATE_COIN_UP){
     // Coin is already up, need to check when to put it down.
-    Serial.println("Coin is held up. check if need to put down");
+    //Serial.println("Coin is held up. check if need to put down");
     if( millis() - coinPreviousMillis > COIN_INTERVAL) {
       //Times Up, put the coint down
       machineCoinState = STATE_COIN_DOWN;
-      digitalWrite(SHROOM_VER_POWER_PIN, HIGH);
-      Serial.println("Times up put coin down. >>>>>>>>>>>>>>>");
-      delay(100);
+      digitalWrite(COIN_MOTOR_POWER_PIN, HIGH);
+     // Serial.println("Times up put coin down. >>>>>>>>>>>>>>>");
     }
   }
    
-  // 
-  // 	if(digitalRead(FLOWER_PUSH_PIN)){
-  //		if(machineFlowerState == STATE_FLOWER_DOWN){
-  //			digitalWrite(FLOWER_MOTOR_POWER_PIN, HIGH);
-  //			machineFlowerState = STATE_FLOWER_UP;
-  //			flowerPreviousMillis = millis();
-  //		}else{
-  //			if( millis() - flowerPreviousMillis > FLOWER_INTERVAL) {
-  //				machineFlowerState = STATE_FLOWER_DOWN;
-  //				digitalWrite(FLOWER_MOTOR_POWER_PIN, LOW);
-  //			}
-  //		}
-  //	}	
-  // 
-  //	if(digitalRead(SHROOM_PUSH_PIN)){
-  //
-  //	}
-}
+//CODE BELOW IS FOR FLOWER STUFF
 
-void readPushButtons(){
-  for(int pin=0; pin<3 ; pin++){
-    pushButtonValues[pin] = digitalRead(pushButtonPins[pin]);
+  if ( digitalRead(FLOWER_PUSH_PIN)){
+    //Serial.println("flower button pushed");
+  }else{
+    //Serial.println("flower button not pushed");
   }
-}
+
+  if(digitalRead(FLOWER_PUSH_PIN) && machineFlowerState == STATE_FLOWER_DOWN){
+    // Move the flower up only when the flower is down.
+    //Serial.println("flower button pushed move flower up");
+    machineFlowerState = STATE_FLOWER_UP;
+    flowerPreviousMillis = millis();
+    digitalWrite(FLOWER_MOTOR_POWER_PIN, LOW);
+  }
+
+  if(machineFlowerState == STATE_FLOWER_UP){
+    // flower is already up, need to check when to put it down.
+    //Serial.println("flower is held up. check if need to put down");
+    if( millis() - flowerPreviousMillis > FLOWER_INTERVAL) {
+      //Times Up, put the flowert down
+      machineFlowerState = STATE_FLOWER_DOWN;
+      digitalWrite(FLOWER_MOTOR_POWER_PIN, HIGH);
+      //Serial.println("Times up put flower down. >>>>>>>>>>>>>>>");
+    }
+   }
+
+//CODE BELOW IS FOR SHROOM STUFF
+  if ( digitalRead(SHROOM_PUSH_PIN)){
+    Serial.println("shroom button pushed");
+  }else{
+    Serial.println("shroom button not pushed");
+  }
+
+  if(digitalRead(SHROOM_PUSH_PIN) && machineShroomVerState == STATE_SHROOM_DOWN){
+    // Move the shroom up only when the shroom is down.
+    Serial.println("shroom button pushed move shroom up");
+    machineShroomVerState = STATE_SHROOM_UP;
+    shroomPreviousMillis = millis();
+    digitalWrite(SHROOM_VER_POWER_PIN, LOW);
+  }
+
+  if(machineShroomVerState == STATE_SHROOM_UP){
+    // shroom is already up, need to check when to put it down.
+    Serial.println("shroom is held up. check if need to put down");
+    if( digitalRead(SHROOM_SENSOR_LEFT_PIN) {
+      //Shroom reach to the right, should bring it down now.
+      machineShroomVerState = STATE_SHROOM_DOWN;
+      digitalWrite(SHROOM_VER_POWER_PIN, HIGH);
+      Serial.println("Times up put shroom down. >>>>>>>>>>>>>>>");
+    }
+  }
+
+} //end of loop
+
+//void readPushButtons(){
+//  for(int pin=0; pin<3 ; pin++){
+//    pushButtonValues[pin] = digitalRead(pushButtonPins[pin]);
+//  }
+//}
 
 void printPushButtonsValues(){
   for(int pin =0; pin<3 ;pin++){
